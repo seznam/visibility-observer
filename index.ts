9d0924d3bb2eb6212e0653e23f1d2aabca26ea79
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-12-11 09:13:33 +0100

    added options normalization, documented parts of the code

--- index.ts
+++ index.ts
@@ -7,7 +7,11 @@ export interface IOptions {
   once?: boolean
 }
 
-interface IObservedElementOptions extends IOptions {
+interface INormalizedOptions extends Required<IOptions> {
+  rootMargin: string
+}
+
+interface IObservedElementOptions extends INormalizedOptions {
   unobserve: () => void
 }
 
@@ -26,20 +30,21 @@ export function observe<E extends Element>(
   callback: (visibilityEntry: IntersectionObserverEntry) => void,
   options: IOptions = {},
 ): void {
+  const normalizedOptions = normalizeOptions(options)
   const elementCallbacks = observedElements.get(target)
   const existingConfiguration = elementCallbacks.get(callback)
   if (existingConfiguration) {
-    if (areOptionsEqual(options, existingConfiguration)) {
+    if (areOptionsEqual(normalizedOptions, existingConfiguration)) {
       return
     }
 
     unobserve(target, callback)
   }
 
-  const observeElement = getObserver(options)
+  const observeElement = getObserver(normalizedOptions)
   const unobserveCallback = observeElement(target, options.once ? onceObserver.bind(null, callback) : callback)
   elementCallbacks.set(callback, {
-    ...options,
+    ...normalizedOptions,
     unobserve: unobserveCallback,
   })
 }
@@ -61,7 +66,14 @@ export function unobserve(
   }
 }
 
-function areOptionsEqual(options1: IOptions, options2: IOptions): boolean {
+/**
+ * Performs equality checks for the provided options objects.
+ *
+ * @param options1 The first options object.
+ * @param options2 The second options object.
+ * @return <code>true</code> iff the options objects are considered strictly equal to each other.
+ */
+function areOptionsEqual(options1: INormalizedOptions, options2: INormalizedOptions): boolean {
   return (
     options1.rootMargin === options2.rootMargin &&
     options1.threshold === options2.threshold &&
@@ -69,6 +81,17 @@ function areOptionsEqual(options1: IOptions, options2: IOptions): boolean {
   )
 }
 
+/**
+ * Wrapper for a listener callback used to make the provided callback being executed only once when the observed
+ * element becomes visible and then being unregistered.
+ *
+ * This function is not meant to be used on it's own, it's expected to be bound to the actual callback to invoke and
+ * then passed to the underlying observer.
+ *
+ * @param callback The callback to execute once the observed element is visible and should no longer be executed after
+ *        that.
+ * @param entry The observer's entry, provided by the observer when a change to the element's visibility is detected.
+ */
 function onceObserver(callback: Listener, entry: IntersectionObserverEntry): void {
   if (entry.isIntersecting) {
     unobserve(entry.target, callback)
@@ -76,21 +99,49 @@ function onceObserver(callback: Listener, entry: IntersectionObserverEntry): voi
   }
 }
 
-function getObserver(options: IOptions): ObserveCallback {
-  const rootMargin = typeof options.rootMargin !== 'undefined' ? options.rootMargin : '0px 0px 0px 0px'
-  const normalizedRootMargin = typeof rootMargin === 'number' ? `${rootMargin}px` : rootMargin
-  const threshold = typeof options.threshold !== 'undefined' ? options.threshold.toString() : 0
-  const serializedOptions = `${normalizedRootMargin};${threshold};${!!options.once}`
+/**
+ * Returns the insular observer to use for the provided options. The options are used as cache keys to re-use the
+ * existing observers as much as possible.
+ *
+ * @param options The observer options, configuring its behavior.
+ * @return The observer to use with the provided options.
+ */
+function getObserver(options: INormalizedOptions): ObserveCallback {
+  const serializedOptions = `${options.rootMargin};${options.threshold};${options.once}`
   const observer = observers.get(serializedOptions)
   if (observer) {
     return observer
   }
 
-  const normalizedOptions: IOptions & {rootMargin: string} = {
-    ...options,
-    rootMargin: normalizedRootMargin,
-  }
-  const newObserver = insularObserverFactory(IntersectionObserver, normalizedOptions)
+  const newObserver = insularObserverFactory(IntersectionObserver, options)
   observers.set(serializedOptions, newObserver)
   return newObserver
+}
+
+function normalizeOptions(options: IOptions): INormalizedOptions {
+  return {
+    once: !!options.once,
+    rootMargin: normalizeRootMargin(typeof options.rootMargin !== 'undefined' ? options.rootMargin : '0px 0px 0px 0px'),
+    threshold: typeof options.threshold !== 'undefined' ? options.threshold : 0,
+  }
+}
+
+function normalizeRootMargin(rootMargin: number | string) {
+  const parts = (typeof rootMargin === 'number' ? `${rootMargin}px` : rootMargin).trim().split(/\s+/)
+  if (parts.length > 4) {
+    throw new TypeError(
+      'Invalid root margin, expected a single number or a string containing 1 to 4 numbers suffixed by "px" or "%" ' +
+      `and separated by whitespace: ${rootMargin}`,
+    )
+  }
+  if (parts.length === 1) {
+    parts.push(parts[0])
+  }
+  if (parts.length === 2) {
+    parts.push(parts[0])
+  }
+  if (parts.length === 3) {
+    parts.push(parts[1])
+  }
+  return parts.join(' ')
 }
